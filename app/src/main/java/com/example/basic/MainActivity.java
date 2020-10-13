bc384c2c6044967548266d599ca01617d1fb7e71
neil <nilnil47@gmail.com>
2020-10-13 20:23:13 +0300

    in the middle of grpc example

--- app/src/main/java/com/example/basic/MainActivity.java
+++ app/src/main/java/com/example/basic/MainActivity.java
@@ -3,12 +3,74 @@ package com.example.basic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import messaging.MapleServiceGrpc;
+import messaging.Service;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "test";
+    private String host;
+    private int port;
+    private MapleServiceGrpc.MapleServiceStub asyncStub;
+    private MapleServiceGrpc.MapleServiceBlockingStub blockingStub;
+
+
+    void startStreaming() {
+        Log.d(TAG, "startStreaming");
+        Service.Empty request = Service.Empty.newBuilder().build();
+//        Service.Empty empty = blockingStub.sendChatMessage(Service.ChatMessage.newBuilder().setId(1).setMessage("aaa").build());
+        GrpcTask
+        asyncStub.startChatMessageStreaming(request, new StreamObserver<Service.ChatMessage>() {
+            @Override
+            public void onNext(Service.ChatMessage value) {
+                Log.d(TAG, "onNext: " + value.getMessage());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.d(TAG, "onError: " + t);
+            }
+
+            @Override
+            public void onCompleted() {
+                Log.d(TAG, "onCompleted: ");
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        savedInstanceState.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        host = "192.168.1.18";
+        port = 50051;
+        ManagedChannel mChannel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        asyncStub = MapleServiceGrpc.newStub(mChannel);
+        mChannel.
+//        blockingStub = MapleServiceGrpc.newBlockingStub(mChannel);
+
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startStreaming();
+            }
+        });
+
+
+
+
+
+
+//        StartChatMessageStreaming.newBuilder.;
     }
 }
